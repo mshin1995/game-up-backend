162de82d55a74b1a8f36ed791e55900af1bcac40
github username <github email address>
2019-06-06 19:01:07 -0700

    routes1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :lists, only: [:update, :destroy, :create, :indexx]
+  resources :lists, only: [:update, :destroy, :create, :index]
   resources :users
   get '/lists/:user_id', to: 'lists#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
